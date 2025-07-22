c9dbe288b30cb621b78cfb6efe3454f4eeec9dfc
jeffersonholandadev <jeffersoneanalimeira@gmail.com>
2025-07-21 22:08:20 -0300

    meu site

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let circulo = document.querySelector(".circulo");
+
+function trocarcor(cor) {
+circulo.style.background = cor;
+    // Additional functionality can be added here to change the color of the cup
+}
+let imagem = document.querySelector(".copo-grande-verde ");
+
+function trocarimagem (img){
+    imagem.src = img
+   
+
+}
+
+
+    
+    // Additional functionality can be added here to change the image of the cup
+
+
